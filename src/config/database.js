b47a7db6096527ebc75af20e71b98474e836ac9d
LucasBGarcia <barbozagarcia@yahoo.com.br>
2024-04-16 14:56:57 -0300

    refact

--- src/config/database.js
+++ src/config/database.js
@@ -1,13 +1,3 @@
-// module.exports = {
-//     username: process.env.NAME,
-//     dialect: process.env.DIALECT,
-//     host: process.env.HOST,
-//     database: process.env.DATABASE,
-//     define: {
-//         timestamps: true,
-//     },
-// }
-
 module.exports = {
     username: process.env.DB_USER,
     dialect: process.env.DB_DIALECT,
@@ -19,18 +9,5 @@ module.exports = {
     JWT_KEY: process.env.JWT_KEY
 };
 
-// module.exports = {
-//     username: 'easycaixa',
-//     dialect: 'mysql',
-//     // dialect: process.env.DB_DIALECT,
-//     host: '54.233.123.29',
-//     password: 'Lucasbg1!',
-//     database: 'trocavidro',
-//     port: 3306,
-//     define: {
-//         timestamps: true,
-//     },
 
-//     JWT_KEY: 'SDSAUDSADUYDG * SADSADDASD * ASDSD'
-// };
 
